bf3cb5f2c1669968ec2c371fc12a293045794e2d
wertrain <wertrain@gmail.com>
2020-10-01 23:57:25 +0900

    WIP

--- ConsoleFallingBlockPuzzle/Program.cs
+++ ConsoleFallingBlockPuzzle/Program.cs
@@ -10,15 +10,8 @@ namespace ConsoleFallingBlockPuzzle
     {
         static void Main(string[] args)
         {
-            //var field = new Field(10, 30);
-            //Drawer.Draw(field, true, 10);
-
-            //var block = Blocks.GetBlocks(Blocks.Types.L);
-            //Drawer.Draw(block, 0);
-
             var game = new Game();
             game.Update();
-            //Drawer.Draw(game.Screen);
         }
     }
 }
